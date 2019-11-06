adae9136e91fa72165904aa520f55c188ea6f0bb
Mary Kate Crittenden <critt102@mail.chapman.edu>
2019-11-06 02:42:44 +0000

    complete

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+/** main.cpp
+ *Main file for registrar's office simulation
+ *@author Mary Kate Crittenden
+ *2278514
+ *critt102@mail.chapman.edu
+ *CPSC 350-01
+ *Assignment 4
+ */
 #include "registrar.h"
 using namespace std;
 
+/**
+ * main method, implemetns and runs registrar.h
+ * @param  argc numer of arguments
+ * @param  argv command lines arguments
+ * @return 0 to end main call
+ */
 int main(int argc, char**argv){
-  Registrar registrar1(argv[1]);
-  registrar1.runSim();
-  registrar1.calc();
+  //if there is no command line argument
+  if(argv[1]==NULL){
+    cout<<"Please Enter a valid file name"<<endl;
+  }
+  //take in file name from command line
+  else{
+    Registrar registrar1(argv[1]);
+    registrar1.runSim();
+    registrar1.calc();
+  }
   return 0;
 }
